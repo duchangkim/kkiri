bc94c0b9d08f8b7933c26f85a69fa8e3aa9e6378
duchang <duchang.dev@gmail.com>
2020-11-12 23:30:54 +0900

    서버 몽고디비 atlas 로 변경

--- src/containers/auth/RegisterCoupleForm.js
+++ src/containers/auth/RegisterCoupleForm.js
@@ -1,35 +1,36 @@
-import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import {
   changeField,
   initializeForm,
   registercouple,
   createCoupleSet,
-} from "../../modules/auth";
-import AuthForm from "../../components/auth/AuthForm";
-import { check } from "../../modules/member";
-import { withRouter } from "react-router-dom";
+} from '../../modules/auth';
+import AuthForm from '../../components/auth/AuthForm';
+import { check } from '../../modules/member';
+import { withRouter } from 'react-router-dom';
 
 const CoupleCodeForm = ({ history }) => {
   const [error, setError] = useState(null);
   const dispatch = useDispatch();
   const { form, auth, authError, otherMember, isSuccess } = useSelector(
     ({ auth }) => {
-      console.log(auth)
-      return({
-      form: auth.registercouple,
-      auth: auth.auth,
-      authError: auth.authError,
-      otherMember: auth.registercouple.otherMember,
-      isSuccess: auth.registercouple.isSuccess,
-    })}
+      console.log(auth);
+      return {
+        form: auth.registercouple,
+        auth: auth.auth,
+        authError: auth.authError,
+        otherMember: auth.registercouple.otherMember,
+        isSuccess: auth.registercouple.isSuccess,
+      };
+    }
   );
 
   const onChange = (e) => {
     const { value, name } = e.target;
     dispatch(
       changeField({
-        form: "registercouple",
+        form: 'registercouple',
         key: name,
         value,
       })
@@ -39,34 +40,34 @@ const CoupleCodeForm = ({ history }) => {
   const onSubmit = (e) => {
     e.preventDefault();
     const { couplecode } = form;
-    console.log("casdfasfasdfadsfasdfadsfouplecode");
+    console.log('casdfasfasdfadsfasdfadsfouplecode');
     console.log(couplecode);
-    if (couplecode === "") {
-      setError("코드를 입력하세요.");
+    if (couplecode === '') {
+      setError('코드를 입력하세요.');
       return;
     }
     dispatch(registercouple(couplecode));
   };
 
   useEffect(() => {
-    dispatch(initializeForm("registercouple"));
+    dispatch(initializeForm('registercouple'));
   }, [dispatch]);
 
   useEffect(() => {
     if (authError) {
       if (authError.response.status === 409) {
-        setError("이미 존재하는 계정입니다.");
+        setError('이미 존재하는 계정입니다.');
         return;
       }
-      setError("커플 인증 실패");
+      setError('커플 인증 실패');
       return;
     }
     if (auth) {
-      console.log("커플 인증 성공");
+      console.log('커플 인증 성공');
       console.log(auth);
       dispatch(check());
     }
-    setError("");
+    setError('');
   }, [auth, authError, dispatch]);
 
   useEffect(() => {
@@ -75,20 +76,20 @@ const CoupleCodeForm = ({ history }) => {
       console.log(otherMember._id);
       dispatch(createCoupleSet(otherMember._id));
     } else if (auth.coupleShareCode) {
-      history.push("/kkiri/home");
+      history.push('/kkiri/home');
       return;
     }
-  }, [dispatch,auth, otherMember]);
-  
+  }, [dispatch, auth, otherMember, history]);
+
   useEffect(() => {
     if (auth.coupleShareCode) {
-      history.push("/kkiri/home");
+      history.push('/kkiri/home');
       return;
     }
   }, [history, auth]);
 
   if (!auth.userCode) {
-    return <h1>꺼지게</h1>
+    return <h1>꺼지게</h1>;
   }
 
   return (
