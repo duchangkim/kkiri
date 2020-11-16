b73baa387fe6191adc622c7b32411c69dd3bd83c
duchang <duchang.dev@gmail.com>
2020-11-16 14:58:33 +0900

    20201116 채팅 80% 완성

--- src/lib/api/chat.js
+++ src/lib/api/chat.js
@@ -1,3 +1,4 @@
 import client from "./client";
 
 export const getMessageList = ({ limit }) => client.get(`/api/chat/list/${limit}`);
+export const insertMessageList = (messageList) => client.post('/api/chat/list', messageList)
