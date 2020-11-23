193c9db5e7f30c0e50053a2cd3273ab7a81245f3
duchang <duchang.dev@gmail.com>
2020-11-23 10:47:28 +0900

    20201123 메인 디데이추가 수정

--- src/containers/main/LeftMainContainer.js
+++ src/containers/main/LeftMainContainer.js
@@ -30,15 +30,15 @@ const LeftMainContainer = () => {
   };
   const handleGetTogetherDateClick = (e) => {
     e.preventDefault();
-    // console.dir(dateInputValue);
-
+    if(!dateInputValue) {
+      alert('날짜를 선택해주세요!')
+      return;
+    }
     dispatch(insertGetTogetherDate(dateInputValue));
   };
 
   useEffect(() => {
     if (!myWeather) {
-      // console.log('ㅅㅂ마이웨더좀 불러와라');
-      // console.log(`이거슨 커플님 아이디 : ${member.coupleId}`);
       dispatch(getCouple(member.coupleId));
       getPosition(dispatch, getMyWeather);
     }
@@ -46,7 +46,6 @@ const LeftMainContainer = () => {
 
   useEffect(() => {
     if (!yourWeather) {
-      // console.log('느그 날씨좀 불러와랏 ㅂ');
       if (couple) {
         const API_KEY = "8838396b78d2bd1ab29b19d58374f16c";
         dispatch(
