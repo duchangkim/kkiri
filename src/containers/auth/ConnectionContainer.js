890750e0dbd286259aed18e5b9f3423c69baffff
duchang <duchang.dev@gmail.com>
2020-11-27 15:22:40 +0900

    20201127 회원가입 로딩 페이지 추가

--- src/containers/auth/ConnectionContainer.js
+++ src/containers/auth/ConnectionContainer.js
@@ -1,19 +1,21 @@
-import React, { useEffect, useState } from "react";
-import { withRouter } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
-import { check, logout } from "../../modules/member";
+import React, { useEffect, useState } from 'react';
+import { withRouter } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { check, logout } from '../../modules/member';
 import {
   changeField,
   initializeForm,
   findOtherMember,
   createCoupleSet,
   superInitialize,
-} from "../../modules/auth";
-import Connection from "../../components/Auth/Connection";
+} from '../../modules/auth';
+import Connection from '../../components/Auth/Connection';
+import LoadingPage from '../../pages/LoadingPage';
 
 const ConnectionContainer = ({ history }) => {
   const dispatch = useDispatch();
-  const [errorMessage, setErrorMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState('');
+  const [loading, setLoading] = useState(false);
   const { form, otherMember, otherMemberError, auth } = useSelector(
     ({ auth }) => ({
       form: auth.connection,
@@ -32,12 +34,12 @@ const ConnectionContainer = ({ history }) => {
     const { name, value } = e.target;
     dispatch(
       changeField({
-        form: "connection",
+        form: 'connection',
         key: name,
         value,
       })
     );
-    setErrorMessage("");
+    setErrorMessage('');
   };
 
   const handleSubmit = (e) => {
@@ -45,38 +47,40 @@ const ConnectionContainer = ({ history }) => {
     // console.log(member.userCode)
     // console.log(Number(form.otherUserCode))
 
-    if(member.userCode === Number(form.otherUserCode)) {
-      console.log('혼자서는 연애할 수 없다')
-      setErrorMessage('본인 고유번호는 입력하실 수 없습니다')
+    if (member.userCode === Number(form.otherUserCode)) {
+      // console.log('혼자서는 연애할 수 없다')
+      setErrorMessage('본인 고유번호는 입력하실 수 없습니다');
     } else {
+      setLoading(true);
       dispatch(findOtherMember(form.otherUserCode));
     }
   };
 
   const onLogout = () => {
     dispatch(logout());
-    history.push("/");
+    history.push('/');
   };
 
   useEffect(() => {
     dispatch(check());
-    dispatch(initializeForm("connection"));
+    dispatch(initializeForm('connection'));
 
     return () => {
-      dispatch(initializeForm("connection"));
-      setErrorMessage("");
+      setLoading(false);
+      dispatch(initializeForm('connection'));
+      setErrorMessage('');
     };
   }, [dispatch]);
 
   useEffect(() => {
     if (otherMemberError) {
       if (otherMemberError.response.status === 401) {
-        setErrorMessage("상대방을 찾을 수 없습니다.");
+        setErrorMessage('상대방을 찾을 수 없습니다.');
       }
     }
 
     if (otherMember) {
-      console.log("커플연결 진행시켜");
+      // console.log("커플연결 진행시켜");
       const { _id } = otherMember;
       dispatch(createCoupleSet(_id));
     }
@@ -85,24 +89,26 @@ const ConnectionContainer = ({ history }) => {
   useEffect(() => {
     if (auth) {
       if (auth.coupleShareCode) {
-        console.log("연결 성공일세");
+        // console.log("연결 성공일세");
         dispatch(check());
-        history.push("/kkiri/home");
+        history.push('/kkiri/home');
         dispatch(superInitialize());
       }
     }
   }, [auth, history, dispatch]);
 
   if (!member) {
-    history.push("/");
+    history.push('/');
     return <h3>잉</h3>;
   }
 
   if (member) {
     if (member.userCode === null || !member.userCode) {
-      history.push("/kkiri/home");
+      // setLoading(false);
+      history.push('/kkiri/home');
     }
   }
+
   return (
     <Connection
       member={member}
